ee9d30a24ddfbab788d9646d31bc8f9df5d7db41
Hrita <hritadutta@gmail.com>
2026-09-09 01:04:41 +0545

    feat: add request middleware

--- server/server.js
+++ server/server.js
@@ -2,9 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const db = require("./database/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+app.use(express.json());
 
 // DB connection
 db();
