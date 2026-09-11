cf6045c41a7f008a353e5256fac3cd48fdfadd1f
Hrita <hritadutta@gmail.com>
2026-09-12 00:46:22 +0545

    fix: correct auth page redirect logic

---
+++ client/src/components/common/CheckAuth.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router";
+
+const isAuthPage =
+  location.pathname.includes("/login") ||
+  location.pathname.includes("/register");
+
+// check authentication and authorization
+const CheckAuth = ({ isAuthenticated, user, children }) => {
+  const location = useLocation();
+
+  // not authenticated user
+  if (!isAuthenticated && !isAuthPage) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  // authenticated user
+  if (isAuthenticated && isAuthPage) {
+    // navigate according to user role
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop" />;
+    }
+  }
+
+  // Navigate to unauth page
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.startsWith("/admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.startsWith("/shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  return <>{children}</>;
+};
+
+export default CheckAuth;
