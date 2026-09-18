9f2b1f21524423fce223b0815f6d325a1bb1bf99
Hrita <hritadutta@gmail.com>
2026-09-18 23:54:56 +0545

    feat: add check-auth route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,10 +4,18 @@ const {
   loginUser,
   logoutUser,
 } = require("../controllers/auth/auth.controller");
+const authMiddleware = require("../middleware/auth-middleware");
 
 router.post("/register", registerUser);
-router.post("/register", loginUser);
+router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.get("/check-auth", auth)
+router.get("/check-auth", authMiddleware, (req, res) => {
+  const user = req.userInfo;
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    user: user,
+  });
+});
 
 module.exports = router;
