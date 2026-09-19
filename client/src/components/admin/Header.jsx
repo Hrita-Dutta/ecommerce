56a95a442da6076e00c3ef480068ea4d8f28fce8
Hrita <hritadutta@gmail.com>
2026-09-19 20:56:42 +0545

    feat: add admin header

--- client/src/components/admin/Header.jsx
+++ client/src/components/admin/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { LogOut, Menu } from "lucide-react";
 
 const AdminHeader = () => {
-  return <div>AdminHeader</div>;
+  return (
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button className="lg:hidden sm:block">
+        <Menu />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1  justify-center">
+        <span className="text-2xl font-semibold">Welcome to Admin Dashboard</span>
+      </div>
+      <div className="flex justify-end">
+        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
+          Logout <LogOut />
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default AdminHeader;
